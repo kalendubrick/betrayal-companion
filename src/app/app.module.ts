89a2fbf421e2ff2c3dde98f801e06207d8fa1778
Kalen Dubrick <kalen3285@gmail.com>
2017-02-04 19:26:22 -0600

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
+import 'hammerjs';
 
 import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
