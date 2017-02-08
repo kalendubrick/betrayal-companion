8ec685037784689512e750d9bb05056b59c1446d
Kalen Dubrick <kalen3285@gmail.com>
2017-02-07 20:24:36 -0600

    Add route for github-pages deploy, where the app is not at /.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'betrayal-companion', component: HomeComponent }
 ]
